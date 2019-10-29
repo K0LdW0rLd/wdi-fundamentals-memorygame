f230a9e02c60c7d4a558079975fef740ec85e290
Kelly Collins <koldworld@Kellys-MacBook-Pro.local>
2019-10-29 06:52:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2){
+  // Executes if length is equal to 2
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert('Sorry, try Again.');
+  }
+}
