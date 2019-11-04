fbe47ca79be0b17153753d3da2c70989b90f7845
Kelly Collins <koldworld@Kellys-MacBook-Pro.local>
2019-11-03 22:01:47 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,22 @@ const cards = [
   {
     rank: 'queen',
     suit: 'hearts',
-    cardImage: 'images/queen-of-hearts.png'
+    cardImage: 'CSS/images/queen-of-hearts.png'
   },
   {
     rank: 'queen',
     suit: 'diamonds',
-    cardImage: 'images/queen-of-diamonds.png'
+    cardImage: 'CSS/images/queen-of-diamonds.png'
   },
   {
     rank: 'king',
     suit: 'hearts',
-    cardImage: 'images/king-of-hearts.png'
+    cardImage: 'CSS/images/king-of-hearts.png'
   },
   {
     rank: 'king',
     suit: 'diamonds',
-    cardImage: 'images/king-of-diamonds.png'
+    cardImage: 'CSS/images/king-of-diamonds.png'
   }
 ];
 const cardsInPlay = [];
@@ -28,15 +28,31 @@ function checkForMatch() {
     alert('Sorry, try Again.');
   }
 }
-function flipCard(cardId) {
-  console.log('User flipped ' + cards[cardId].rank);
+function flipCard() {
+  let cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
+  console.log('User flipped ' + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
   if (cardsInPlay.length === 2){
     // Executes if length is equal to 2
     checkForMatch();
   }
+
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'CSS/images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard)
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+
+
+createBoard();
